503d75e09e276ae2d36b903d422a5e40f52292bc
ILIDANUM <ILIDANUM@yandex.ru>
2024-02-28 00:56:32 +0300

    test_arrs with fixture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,13 +1,22 @@
+import pytest
+
 from utils import arrs
 
+list_coll = [1, 2, 3, 4]
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+
+@pytest.fixture
+def coll():
+    return list_coll.copy()
+
+
+def test_get(coll):
+    assert arrs.get(coll, 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(coll):
+    assert arrs.my_slice(coll, 1, 3) == [2, 3]
+    assert arrs.my_slice(coll, 1) == [2, 3, 4]
     assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1, 2, 3]) == [1, 2, 3]
+    assert arrs.my_slice(coll) == [1, 2, 3, 4]
